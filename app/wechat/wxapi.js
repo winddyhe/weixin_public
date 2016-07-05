6285f6931907509e1385e19f25e335e267bc5d26
winddyhe <hgplan@126.com>
2016-07-06 00:39:17 +0800

    客服功能。。。

--- app/wechat/wxapi.js
+++ app/wechat/wxapi.js
@@ -1,16 +1,33 @@
 var wechat      = require('wechat-api');
-
 var wx_config   = require('./config.json');
 
 var wxAppID = wx_config.wxAppID;
 var wxAppSecret = wx_config.wxAppSecret;
+var wxLogoPath = wx_config.logoPath;
 
 var wxApi = new wechat(wxAppID, wxAppSecret);
 
-function wx_create_menu() {
+var WxApi = function(){ };
+module.exports = new WxApi();
+
+/**
+ * 创建微信菜单
+ */
+WxApi.prototype.create_menu = function () {
     wxApi.createMenu(wx_config.wxMenu, function(err, result){
-        console.log('---- Winddy: ', result);
+        console.log('---- Winddy: create menu, ', result);
     });
 };
 
-module.exports = wx_create_menu;
+WxApi.prototype.add_kf_account = function(){
+    wxApi.addKfAccount('winddyhe@gh_24c9c11c200d', '小爱', 'naruto78981223', function(err, result){
+        console.log('---- Winddy: add kf, ', result);
+
+        // wxApi.getCustomServiceList(function (err, result) {
+        //     console.log('---- Winddy: add kf, ', result);
+        // });
+        // wxApi.sendText('winddyWeiXinCourse', "Hello world..你好", function (err, result) {
+        //     console.log('---- Winddy: add kf, ', result);
+        //});
+    });
+};
